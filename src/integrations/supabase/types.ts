d542b5386c72bb47ef1a4943bc2d75bad9508574
Br3nzi11 <Brenzill.McMaster@gosolutions.group>
2026-04-24 09:56:16 +0200

    new updated

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -6,7 +6,7 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[]
 
-export type Database = {
+export type  Database = {
   // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
